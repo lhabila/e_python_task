0e3f8dbbe1ab770065045ec5ee96233598de8f64
levihabila <lhabila@gmail.com>
2023-06-12 00:26:12 +0300

    Introduce a random delay between 0 and 1 second

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,6 +8,7 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
+
 async def async_fib(n):
     await asyncio.sleep(random.uniform(0, 1))
     return fib(n)
@@ -17,18 +18,7 @@ async def main():
     n = int(input("Enter a positive number: "))
     task1 = asyncio.create_task(async_fib(n))
     task2 = asyncio.create_task(async_fib(n))
-    await asyncio.gather(task1, task2)  # Wait for both tasks to finish
-    result1 = task1.result()
-    result2 = task2.result()
 
-    print(f"Fibonacci number Fib({n}): {result1}")
-    if result1 == result2:
-        print("Both tasks finished at the same time.")
-    elif result1 < result2:
-        print("Task 1 finished first.")
-    else:
-        print("Task 2 finished first.")
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    tasks = [task1, task2]
+    finished, _unfinished = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    finished = finished.pop()
